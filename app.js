5226071a4ea2ce6a0de357a0d65884cc3b2f53b2
Jafmatute <josueari20@gmail.com>
2021-08-23 18:10:16 -0600

    Despliegue app Lun, 23 agosto

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 /** @format */
 //importaciones
 const express = require("express");
-const hbs = require("hbs");
 require("dotenv").config();
 
 //express
 const app = express();
 const port = process.env.PORT;
 
-//hbs(HandleBars)
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-
 //Mostrar index html
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", user());
-});
-
-//Rutas de la página web
-app.get("/generic", (req, res) => {
-  res.render("generic", user());
-});
-
-app.get("/elements", (req, res) => {
-  res.render("elements", user());
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
@@ -33,10 +19,3 @@ app.listen(port, () => {
     `Su proyecto está corriendo en el puerto http://localhost:${port}`
   );
 });
-
-function user() {
-  return {
-    nombre: "Josue Flores",
-    titulo: "Express -Node",
-  };
-}
